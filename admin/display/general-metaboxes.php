40aab14f8130be2492f8b66cda0741fceb86e435
Arnaud Broes <arnaudbroes@users.noreply.github.com>
2017-03-06 22:00:14 +0100

    Make field & button for Newsletter section translatable #790

--- admin/display/general-metaboxes.php
+++ admin/display/general-metaboxes.php
@@ -103,8 +103,8 @@ class aiosp_metaboxes {
 							<i><?php _e( 'Sign up today and receive a free copy of the e-book 5 SEO Tips for WordPress ($39 value).', 'all-in-one-seo-pack' ); ?></i>
 						</p>
 						<p><input type="text" value="" name="EMAIL" class="required email" id="mce-EMAIL"
-						          placeholder="Email Address">
-							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
+						          placeholder="<?php __( 'Email Address', 'all-in-one-seo-pack' )?>">
+							<input type="submit" value="<?php __( 'Subscribe', 'all-in-one-seo-pack' )?>" name="subscribe" id="mc-embedded-subscribe"
 							       class="btn"></p>
 					</form>
 				</div>
