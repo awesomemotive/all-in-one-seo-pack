e6c18f166c20fb9d119d19757364a7e9b5c76c9b
Arnaud Broes <arnaudbroes@users.noreply.github.com>
2018-03-29 00:34:23 +0200

    Fixed character counter for home title #1581

--- js/modules/aioseop_module.js
+++ js/modules/aioseop_module.js
@@ -75,20 +75,19 @@ function countChars( field, cntfield ) {
     }
 	if ( cntfield.val() > field_size ) {
         cntfield.removeClass().addClass('aioseop_count_ugly');
-	} else if ('aiosp_title' === field.attr('name')) {
+	} else if ( ( 'aiosp_title' === field.attr('name' ) ) || ( 'aiosp_home_title' === field.attr('name') ) ) {
         if ( cntfield.val() > ( field_size - 6 ) ) {
             cntfield.removeClass().addClass('aioseop_count_bad');
         } else {
             cntfield.removeClass().addClass('aioseop_count_good');
         }
-    }
-    else {
+    } else {
         if ( cntfield.val() > ( field_size - 91 ) ) {
             cntfield.removeClass().addClass('aioseop_count_bad');
         } else {
             cntfield.removeClass().addClass('aioseop_count_good');
         }
-	}
+    }
 }
 
 /**
