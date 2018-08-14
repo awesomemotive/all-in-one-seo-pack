84b7f1eb60edfa6ee1c45b92eec72653d9c7bcea
EkoJR <ekojr1337@gmail.com>
2018-08-14 09:40:59 -0700

    WPCS Cleanup

--- js/modules/aioseop_module_footer.js
+++ js/modules/aioseop_module_footer.js
@@ -24,29 +24,24 @@
 				ui.tooltip.css("min-width", "170px");
 				ui.tooltip.css("max-width", "396px");
 
-				if (typeof(event.originalEvent) === 'undefined')
-				{
+				if ( 'undefined'=== typeof( event.originalEvent ) ) {
 					return false;
 				}
 
-				var $id = $(ui.tooltip).attr('id');
+				var $id = $( ui.tooltip ).attr( 'id' );
 
 				// close any lingering tooltips
-				$('div.ui-tooltip').not('#' + $id).remove();
+				$( 'div.ui-tooltip' ).not( '#' + $id ).remove();
 
 				// ajax function to pull in data and add it to the tooltip goes here
 			},
-			close: function(event, ui)
-			{
-				ui.tooltip.hover(function()
-					{
-						$(this).stop(true).fadeTo(400, 1);
+			close: function( event, ui ) {
+				ui.tooltip.hover( function() {
+						$( this ).stop( true ).fadeTo( 400, 1 );
 					},
-					function()
-					{
-						$(this).fadeOut('400', function()
-						{
-							$(this).remove();
+					function() {
+						$( this ).fadeOut( '400', function() {
+							$( this ).remove();
 						});
 					});
 			},
