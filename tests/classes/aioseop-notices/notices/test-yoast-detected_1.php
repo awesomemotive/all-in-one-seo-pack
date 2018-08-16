cbb4276fb32f42a86a4f5f6bfc7c8429e41d7639
EkoJR <ekojr1337@gmail.com>
2018-08-15 22:06:59 -0700

    Add PHPUnit test for Yoast detected

---
+++ tests/classes/aioseop-notices/notices/test-yoast-detected_1.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class AIOSEOP_Notices_TestCase
+ *
+ * @package All_in_One_SEO_Pack
+ * @subpackage AIOSEOP_Notices
+ * @since 2.4.5.1
+ *
+ * @group AIOSEOP_Notices
+ * @group Admin
+ * @group Notices
+ */
+
+include_once AIOSEOP_UNIT_TESTING_DIR . '\base\class-aioseop-notices-testcase.php';
+
+/**
+ * Class Test Notice - Yoast Detected 1
+ *
+ * @since 2.4.5.1
+ *
+ * @package classes\AIOSEOP_Notices\notices
+ */
+class Test_Notice_YoastDetected_1 extends AIOSEOP_Notices_TestCase {
+
+	/**
+	 * Mock Single Notice
+	 *
+	 * @since 2.4.5.1
+	 *
+	 * @return array
+	 */
+	protected function mock_notice() {
+		return aioseop_notice_yoast_detected_1();
+	}
+}
