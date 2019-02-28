867c79b8831f63878defade01e8bcdf84f90eec4
Ashish Ravi <contactashish13@gmail.com>
2019-02-28 11:26:37 +0530

    phpcs

--- bin/init-grunt.sh
+++ bin/init-grunt.sh
@@ -6,4 +6,6 @@ composer install --no-interaction --ignore-platform-reqs
 composer global require "squizlabs/php_codesniffer"
 
 git clone -b master --depth 1 https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git "$HOME/wordpress-coding-standards"
+phpenv rehash
 phpcs --config-set installed_paths "$HOME/wordpress-coding-standards"
+phpenv rehash
