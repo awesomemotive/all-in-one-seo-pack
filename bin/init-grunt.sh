c8d5e3518ec06cf9ceeab8533b004a25be3620d8
Ashish Ravi <contactashish13@gmail.com>
2019-03-01 16:15:02 +0530

    test

--- bin/init-grunt.sh
+++ bin/init-grunt.sh
@@ -1,12 +1,13 @@
 npm install
+npm install grunt -g
 npm install grunt-cli -g
-npm install grunt grunt-mkdir grunt-phpcs grunt-phpcbf grunt-phplint grunt-contrib-jshint grunt-contrib-uglify grunt-eslint
+npm install grunt-mkdir grunt-phpcs grunt-phpcbf grunt-phplint grunt-contrib-jshint grunt-contrib-uglify grunt-eslint
 
 composer install --no-interaction --ignore-platform-reqs
 
 COMPOSER_LOCATION=$(composer config home --global)
 export PATH="$COMPOSER_LOCATION/vendor/bin:$PATH"
-composer self-update;
+composer self-update
 
 composer global require "squizlabs/php_codesniffer"
 
