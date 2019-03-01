625a54bd515c3f7ae33c7c52df48cfe9a5f3e899
Ashish Ravi <contactashish13@gmail.com>
2019-03-01 15:55:04 +0530

    test

--- bin/init-grunt.sh
+++ bin/init-grunt.sh
@@ -1,8 +1,13 @@
 npm install
 npm install grunt-cli -g
-npm install grunt grunt-mkdir grunt-phpcs grunt-phpcbf grunt-phplint grunt-contrib-jshint grunt-contrib-uglify grunt-eslint
+#npm install grunt grunt-mkdir grunt-phpcs grunt-phpcbf grunt-phplint grunt-contrib-jshint grunt-contrib-uglify grunt-eslint
 
 composer install --no-interaction --ignore-platform-reqs
+
+COMPOSER_LOCATION=$(composer config home --global)
+export PATH="$COMPOSER_LOCATION/vendor/bin:$PATH"
+composer self-update;
+
 composer global require "squizlabs/php_codesniffer"
 
 git clone -b master --depth 1 https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git "$HOME/wordpress-coding-standards"
