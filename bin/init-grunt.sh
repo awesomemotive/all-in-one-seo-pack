ec4a44c93dd882c5e1dfdf2fabbeda45ff18f99a
Ashish Ravi <contactashish13@gmail.com>
2019-03-01 16:41:01 +0530

    test

--- bin/init-grunt.sh
+++ bin/init-grunt.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-npm install
-npm install grunt -g
-npm install grunt-cli -g
-npm install grunt-mkdir grunt-phpcs grunt-phpcbf grunt-phplint grunt-contrib-jshint grunt-contrib-uglify grunt-eslint
+    phpenv config-rm xdebug.ini
 
-composer install --no-interaction --ignore-platform-reqs
+    ./"$HOME/.nvm/nvm.sh"
+    nvm install v8.9.0
+    nvm use v8.9.0
 
-COMPOSER_LOCATION=$(composer config home --global)
-export PATH="$COMPOSER_LOCATION/vendor/bin:$PATH"
-composer self-update
+    npm install
+    npm install grunt-cli -g
 
-composer global require "squizlabs/php_codesniffer"
+    if [ -f "composer.json" ]; then
+        composer selfupdate 1.0.0 --no-interaction
+        travis_retry composer install --no-interaction --ignore-platform-reqs
+    fi
 
-git clone -b master --depth 1 https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git "$HOME/wordpress-coding-standards"
-phpenv rehash
-phpcs --config-set installed_paths "$HOME/wordpress-coding-standards"
-phpenv rehash
+
+    mv node_modules/.bin/which node_modules/.bin/which.backup
+    rvm install 2.2.0 && rvm use 2.2.0
+    mv node_modules/.bin/which.backup node_modules/.bin/which
+    gem install sass
+    phpenv local --unset
+
+
+          COMPOSER_LOCATION=$(composer config home --global)
+          export PATH="$COMPOSER_LOCATION/vendor/bin:$PATH"
+
+
+          composer self-update;
+              composer global require "squizlabs/php_codesniffer=2.9.*|3.3.*"
+
+              git clone -b master --depth 1 https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git "$HOME/wordpress-coding-standards"
+              phpenv rehash
+              phpcs --config-set installed_paths "$HOME/wordpress-coding-standards"
+              phpenv rehash
