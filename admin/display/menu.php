8095d7b057243af165efa80e3f645ec7245de5b0
Arnaud Broes <abroes@outlook.be>
2019-04-23 15:16:55 -0400

    + Open Upgrade link in menu as new tab #773

--- admin/display/menu.php
+++ admin/display/menu.php
@@ -23,6 +23,8 @@ class AIOSEOPAdminMenus {
 		} else {
 			return;
 		}
+
+        add_action( 'admin_head', array( $this, 'pro_submenu_new_tab' ) );
 	}
 
 	function remove_menus() {
@@ -44,6 +46,21 @@ class AIOSEOPAdminMenus {
 			$url,
 		);
 	}
+
+	/*
+	 * Opens Upgrade link in our menu as a new tab.
+	 *
+	 * @since 3.0
+	 */
+    function pro_submenu_new_tab() {
+        ?>
+        <script type="text/javascript">
+            jQuery(document).ready( function($) {
+                $('#toplevel_page_all-in-one-seo-pack-aioseop_class ul li').last().find('a').attr('target','_blank');
+            });
+        </script>
+        <?php
+    }
 }
 
 	new AIOSEOPAdminMenus();
