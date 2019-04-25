ecc7a81b7f706b7e63c3d8e149ca44f2db29c52c
Arnaud Broes <abroes@outlook.be>
2019-04-25 14:09:38 -0400

    = PR comments #773

--- admin/display/menu.php
+++ admin/display/menu.php
@@ -24,9 +24,7 @@ class AIOSEOPAdminMenus {
 			return;
 		}
 
-		// enqueued here because All_in_One_SEO_Pack_Module::admin_enqueue_scripts does not work
-		add_action( 'admin_enqueue_scripts', array($this, 'upgrade_link_aioseop_menu_new_tab' ) );
-		add_action( 'admin_enqueue_scripts', array($this, 'upgrade_link_plugins_menu_new_tab' ) );
+		add_action( 'admin_enqueue_scripts', array($this, 'admin_enqueue_scripts' ) );
 	}
 
 	function remove_menus() {
@@ -50,24 +48,20 @@ class AIOSEOPAdminMenus {
 	}
 
 	/*
-	 * Opens Upgrade link in menu as new tab.
+	 * Opens Upgrade to Pro links in WP Admin as new tab.
+	 *
+	 * Enqueued here because All_in_One_SEO_Pack_Module::admin_enqueue_scripts does not work.
+	 *
+	 * @param string $hook
 	 *
 	 * @since 3.0
 	 */
-	function upgrade_link_aioseop_menu_new_tab() {
+	function admin_enqueue_scripts( $hook ) {
 		wp_enqueue_script( 'aioseop_menu_js', AIOSEOP_PLUGIN_URL . 'js/aioseop-menu.js', array( 'jquery' ), AIOSEOP_VERSION, true );
-	}
 
-	/*
-	 * Opens Upgrade link in plugins menu as new tab.
-	 *
-	 * @since 3.0
-	 */
-	function upgrade_link_plugins_menu_new_tab( $hook ) {
-		if ( 'plugins.php' != $hook ) {
-			return;
+		if ( 'plugins.php' === $hook ) {
+			wp_enqueue_script( 'aioseop_plugins_menu_js', AIOSEOP_PLUGIN_URL . 'js/plugins-menu.js', array( 'jquery' ), AIOSEOP_VERSION, true );
 		}
-		wp_enqueue_script( 'aioseop_plugins_menu_js', AIOSEOP_PLUGIN_URL . 'js/plugins-menu.js', array( 'jquery' ), AIOSEOP_VERSION, true );
 	}
 }
 
--- js/aioseop-menu.js
+++ js/aioseop-menu.js
@@ -1,4 +1,20 @@
-//Opens Upgrade link in menu as new tab.
-jQuery(document).ready( function($) {
-    $('#toplevel_page_all-in-one-seo-pack-aioseop_class ul li').last().find('a').attr('target','_blank');
-});
+/**
+ * AIOSEOP menu.
+ *
+ * Contains all functions that affect the AIOSEOP menu in the sidebar.
+ *
+ * @since  3.0
+ * @package all-in-one-seo-pack
+ */
+(function($) {
+
+    /**
+     * Opens Upgrade to Pro link in AIOSEOP menu as new tab.
+     */
+    function upgrade_link_aioseop_menu_new_tab() {
+        $('#toplevel_page_all-in-one-seo-pack-aioseop_class ul li').last().find('a').attr('target','_blank');
+    }
+
+    upgrade_link_aioseop_menu_new_tab();
+
+}(jQuery));
