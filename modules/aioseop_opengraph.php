1382bbe33911020124db29a4f6d665eec0a2173e
Arnaud Broes <abroes@outlook.be>
2019-05-13 15:30:38 -0400

    Fix undefined $tag in aioseop_opengraph.php #1688

--- modules/aioseop_opengraph.php
+++ modules/aioseop_opengraph.php
@@ -1342,6 +1342,7 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Opengraph' ) ) {
 
 			if ( $type === 'article' && ! empty( $post ) && is_singular() ) {
 				if ( ! empty( $this->options['aiosp_opengraph_gen_tags'] ) ) {
+					$tag = '';
 					if ( ! empty( $this->options['aiosp_opengraph_gen_keywords'] ) ) {
 						$keywords = $aiosp->get_main_keywords();
 						$keywords = $this->apply_cf_fields( $keywords );
