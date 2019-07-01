8cd6265c385cf44c8fe6ee08b0271daf895cbcac
wpsmort <smortiboy@semperfiwebdesign.com>
2019-07-01 16:20:39 -0400

    #435 Adds aria-label for mail list form

--- admin/display/general-metaboxes.php
+++ admin/display/general-metaboxes.php
@@ -133,7 +133,7 @@ class aiosp_metaboxes {
 									placeholder="<?php _e( 'Email Address', 'all-in-one-seo-pack' ); ?>" aria-label="<?php _e( 'Enter your email address', 'all-in-one-seo-pack' ); ?>">
 							<input
 									type="submit" value="<?php _e( 'Subscribe', 'all-in-one-seo-pack' ); ?>" name="subscribe" id="mc-embedded-subscribe"
-									class="button-primary" aria-label="<?php _e( 'Subscribe', 'all-in-one-seo-pack' ); ?>">
+									class="button-primary" aria-label="<?php _e( 'Subscribe to our mailing list', 'all-in-one-seo-pack' ); ?>">
 						</p>
 					</form>
 				</div>
