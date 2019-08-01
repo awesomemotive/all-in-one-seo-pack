24334493aa5f6f39d11b3b250c5587fdaa021c7b
Arnaud Broes <abroes@outlook.be>
2019-08-01 08:28:24 +0200

    Revert changes for count-chars.js #2475

--- js/admin/aioseop-count-chars.js
+++ js/admin/aioseop-count-chars.js
@@ -2,60 +2,85 @@
  * Counts characters of the title and description fields.
  *
  * @since 2.9.2
- * @since 3.2.0 Refactored and moved Preview Snippet code to dedicated file.
+ * @since 3.2.0 Moved to its own file.
  */
 
-$(document).ready( function() {
-	$('.aioseop_count_chars').on('keyup keydown', function(){
-		var inputField = $(this).eq(0);
-		var counter =  $(this).parent().find('[name="' + $(this).attr('data-length-field') + '"]').eq(0);
-		aioseopCountChars( inputField, counter );
+
+var aiosp_title_extra = parseInt( aioseop_count_chars.extraTitleLength, 10 ); // jshint ignore:line
+
+jQuery( document ).ready( function() {
+	aioseopInitCounting();
+});
+
+/**
+ * Preview Snippet
+ *
+ * @since ?
+ * @since 2.9.2 Move from PHP value to JS file
+ */
+jQuery(document).ready( function() {
+	jQuery("#aiosp_title_wrapper").bind("input", function() {
+		jQuery("#aiosp_snippet_title").text(jQuery("#aiosp_title_wrapper input").val().replace(/<(?:.|\n)*?>/gm, ""));
+	});
+	jQuery("#aiosp_description_wrapper").bind("input", function() {
+		jQuery("#aioseop_snippet_description").text(jQuery("#aiosp_description_wrapper textarea").val().replace(/<(?:.|\n)*?>/gm, ""));
 	});
 });
 
 /**
- * The aisoeopCountChars() function.
- * 
- * Counts the characters of a field.
+ * AIOSEOP Init Counting
+ *
+ * @since ?
+ */
+function aioseopInitCounting(){
+	/* count them characters */
+	jQuery( '.aioseop_count_chars' ).on('keyup keydown', function(){
+		aioseopCountChars( jQuery(this).eq(0), jQuery(this).parent().find('[name="' + jQuery(this).attr('data-length-field') + '"]').eq(0));
+	});
+	jQuery( '.aioseop_count_chars' ).each(function(){
+		aioseopCountChars( jQuery(this).eq(0), jQuery(this).parent().find('[name="' + jQuery(this).attr('data-length-field') + '"]').eq(0));
+	});
+}
+
+/**
+ * @summary Counts characters.
  *
  * @since 1.0.0
- * @since 2.9.1 Fix conflict with LearnDash and function name.
- * @since 3.2.0 Minor refactoring.
+ * @since 2.9.1 Fix JS conflict with LearnDash and function name.
  *
- * @param JQuery_Object field
- * @param JQuery_Object counter
+ * @param Object $field.
+ * @param Object $cntfield.
+ * @return Mixed.
  */
-function aioseopCountChars( field, counter ) {
-	var extraTitleLength = parseInt( aioseop_count_chars.extraTitleLength, 10 ); // jshint ignore:line
-	var extraCharacters = 0;
-	var fieldSize = 0;
-
-	if ( ( field.attr('name') === 'aiosp_title' ) && ( typeof extraTitleLength !== 'undefined' ) ) {
-		extraCharacters = extraTitleLength;
+function aioseopCountChars( field, cntfield ) {
+	var extra = 0;
+	var field_size;
+	if ( ( field.attr('name') === 'aiosp_title' ) && ( typeof aiosp_title_extra !== 'undefined' ) ) {
+		extra = aiosp_title_extra;
 	}
-
-	counter.val( field.val().length + extraCharacters );
+	cntfield.val( field.val().length + extra );
 	if ( typeof field.attr('size') !== 'undefined' ) {
-		fieldSize = field.attr('size');
+		field_size = field.attr('size');
 	} else {
-		fieldSize = field.attr('rows') * field.attr('cols');
+		field_size = field.attr('rows') * field.attr('cols');
 	}
-	
-	if ( counter.val() > fieldSize ) {
-		counter.removeClass().addClass('aioseop_count_chars_over_treshold');
-	} 
-	
-	else if ( ( 'aiosp_title' === field.attr('name' ) ) || ( 'aiosp_home_title' === field.attr('name') ) ) {
-		counter.removeClass().addClass('aioseop_count_chars_below_treshold');
-		if ( counter.val() > ( fieldSize - 6 ) ) {
-			counter.removeClass().addClass('aioseop_count_chars_nearing_treshold');
-		} 
-	} 
-	
-	else {
-		counter.removeClass().addClass('aioseop_count_chars_below_treshold');
-		if ( counter.val() > ( fieldSize - 10 ) ) {
-			counter.removeClass().addClass('aioseop_count_chars_nearing_treshold');
+	field_size = parseInt(field_size, 10);
+	if ( field_size < 10 ) {
+		return;
+	}
+	if ( cntfield.val() > field_size ) {
+		cntfield.removeClass().addClass('aioseop_count_ugly');
+	} else if ( ( 'aiosp_title' === field.attr('name' ) ) || ( 'aiosp_home_title' === field.attr('name') ) ) {
+		if ( cntfield.val() > ( field_size - 6 ) ) {
+			cntfield.removeClass().addClass('aioseop_count_bad');
+		} else {
+			cntfield.removeClass().addClass('aioseop_count_good');
+		}
+	} else {
+		if ( cntfield.val() > ( field_size - 10 ) ) {
+			cntfield.removeClass().addClass('aioseop_count_bad');
+		} else {
+			cntfield.removeClass().addClass('aioseop_count_good');
 		}
 	}
 }
