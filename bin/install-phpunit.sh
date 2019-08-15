74f8ba9e64324ab8e3df525c08cca7a8d8fe6c9c
EkoJR <ekojr1337@gmail.com>
2019-08-15 14:02:08 -0700

    Dev-Test syntax

--- bin/install-phpunit.sh
+++ bin/install-phpunit.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-PHPUNIT_VERSION = $1;
-PHP_VERSION     = $2;
+PHPUNIT_VERSION=$1;
+PHP_VERSION=$2;
+
+echo $1;
+echo $PHPUNIT_VERSION;
 
 if [[ 'local' = PHPUNIT_VERSION ]];
 then
