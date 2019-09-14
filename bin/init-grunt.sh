e4af9e0456650cd033c7870dcd56a88c6409a0a3
Arnaud <abroes@outlook.be>
2019-09-14 22:08:22 +0200

    Another test

--- bin/init-grunt.sh
+++ bin/init-grunt.sh
@@ -13,7 +13,8 @@ composer self-update
 
 composer global require "squizlabs/php_codesniffer"
 
-#cgit clone -b master --depth 1 https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git "$HOME/wordpress-coding-standards"
-#cphpenv rehash
+git clone -b master --depth 1 https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git "$HOME/wordpress-coding-standards"
+git clone -b master --depth 1 https://github.com/PHPCompatibility/PHPCompatibility.git "$HOME/phpcompatibility"
+phpenv rehash
 phpcs --config-set installed_paths "$HOME/wordpress-coding-standards,$HOME/phpcompatibility"
 phpenv rehash
