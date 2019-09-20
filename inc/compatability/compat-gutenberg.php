71cbe7b290841b06aee11512fab0eade1c58089a
Arnaud <abroes@outlook.be>
2019-09-20 16:06:03 +0200

    Remove WP v5.3 version check and move function out of action hook #2914

--- inc/compatability/compat-gutenberg.php
+++ inc/compatability/compat-gutenberg.php
@@ -9,10 +9,12 @@
  * @since 3.2.8
  */
 
+chrome_fix_overlapping_metabox();
+
 /**
- * The gutenberg_fix_metabox() function.
+ * The chrome_fix_overlapping_metabox() function.
  *
- * Change height of a specific CSS class to fix an issue in Chrome 77 with Gutenberg.
+ * Fixes a CSS compatibility issue between Gutenberg and Chrome v77 that affects meta boxes.
  *
  * @see https://github.com/WordPress/gutenberg/issues/17406
  * @link https://github.com/semperfiwebdesign/all-in-one-seo-pack/issues/2914
@@ -21,31 +23,44 @@
  *
  * @return void
  */
-function gutenberg_fix_metabox() {
+function chrome_fix_overlapping_metabox() {
 	if ( false !== stripos( $_SERVER['HTTP_USER_AGENT'], 'Chrome/77.' ) ) {
 		add_action(
 			'admin_head',
-			static function () {
-				global $wp_version;
-
-				// Fix should be included in WP v5.3.
-				if ( ! version_compare( $wp_version, '5.0', '>=' ) && version_compare( $wp_version, '5.3', '<' ) ) {
-					return;
-				}
-
-				// CSS class renamed from 'editor' to 'block-editor' in WP v5.2.
-				if ( version_compare( $wp_version, '5.2', '<' ) ) {
-					gutenberg_fix_metabox_helper( 'editor-writing-flow' );
-				} elseif ( version_compare( $wp_version, '5.2', '>=' ) ) {
-					gutenberg_fix_metabox_helper( 'block-editor-writing-flow' );
-				}
-			}
+			'override_gutenberg_css_class'
 		);
 	}
 }
 
 /**
- * The gutenberg_fix_metabox_helper() function.
+ * The override_gutenberg_css_class() function.
+ *
+ * Change height of a specific Gutenberg CSS class.
+ *
+ * @see https://github.com/WordPress/gutenberg/issues/17406
+ * @link https://github.com/semperfiwebdesign/all-in-one-seo-pack/issues/2914
+ *
+ * @since 3.2.8
+ *
+ * @return void
+ */
+function override_gutenberg_css_class() {
+	global $wp_version;
+
+	if ( ! version_compare( $wp_version, '5.0', '>=' ) ) {
+		return;
+	}
+
+	// CSS class renamed from 'editor' to 'block-editor' in WP v5.2.
+	if ( version_compare( $wp_version, '5.2', '<' ) ) {
+		override_gutenberg_css_class_helper( 'editor-writing-flow' );
+	} else {
+		override_gutenberg_css_class_helper( 'block-editor-writing-flow' );
+	}
+}
+
+/**
+ * The override_gutenberg_css_class_helper() function.
  *
  * Overrides a Gutenberg CSS class using inline CSS.
  * Helper method of gutenberg_fix_metabox().
@@ -55,8 +70,6 @@ function gutenberg_fix_metabox() {
  * @param string $class_name
  * @return void
  */
-function gutenberg_fix_metabox_helper( $class_name ) {
+function override_gutenberg_css_class_helper( $class_name ) {
 	echo '<style>.' . $class_name . ' { height: auto; }</style>';
 }
-
-gutenberg_fix_metabox();
