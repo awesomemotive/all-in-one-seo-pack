fc13671b3b62418de302db698a4c2a6af4acc991
Arnaud <abroes@outlook.be>
2019-09-20 16:56:44 +0200

    Fix phpDocs #2914

--- inc/compatability/compat-gutenberg.php
+++ inc/compatability/compat-gutenberg.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * The compat-gutenberg.php file.
- *
  * Contains compatibility fixes for the Gutenberg editor.
  *
  * @package All_in_One_SEO_Pack
@@ -12,8 +10,6 @@
 aioseop_chrome_fix_overlapping_metabox();
 
 /**
- * The chrome_fix_overlapping_metabox() function.
- *
  * Fixes a CSS compatibility issue between Gutenberg and Chrome v77 that affects meta boxes.
  *
  * @see https://github.com/WordPress/gutenberg/issues/17406
@@ -33,8 +29,6 @@ function aioseop_chrome_fix_overlapping_metabox() {
 }
 
 /**
- * The override_gutenberg_css_class() function.
- *
  * Change height of a specific Gutenberg CSS class.
  *
  * @see https://github.com/WordPress/gutenberg/issues/17406
@@ -60,10 +54,7 @@ function aioseop_override_gutenberg_css_class() {
 }
 
 /**
- * The override_gutenberg_css_class_helper() function.
- *
- * Overrides a Gutenberg CSS class using inline CSS.
- * Helper method of gutenberg_fix_metabox().
+ * Overrides a Gutenberg CSS class using inline CSS. Helper method of gutenberg_fix_metabox().
  *
  * @since 3.2.8
  *
