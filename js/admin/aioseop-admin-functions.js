7e33f8c281e9778992b253c22eb03ee1816c2a6f
Arnaud Broes <arnaud.broes@student.howest.be>
2019-11-22 21:17:14 +0100

    Refactor code #3052

--- js/admin/aioseop-admin-functions.js
+++ js/admin/aioseop-admin-functions.js
@@ -7,19 +7,14 @@
  */
 
 /**
- * Checks whether the Classic Editor is active.
+ * Checks whether the Gutenberg Editor is active.
  * 
  * @since 3.3.4
  * 
- * @return bool Whether or not the Classic Editor is active.
+ * @return bool Whether or not the Gutenberg Editor is active.
  */
-function aioseopIsClassicEditor() {
-    if ("undefined" !== typeof tinyMCE) {
-        if (null === tinyMCE.activeEditor || false !== tinyMCE.activeEditor.isHidden()) {
-            return true;
-        }
-    }
-    return false;
+function aioseopIsGutenbergEditor() {
+    return document.body.classList.contains( 'block-editor-page' );
 }
 
 /**
@@ -34,4 +29,82 @@ function aioseopIsVisualTab() {
         return true;
     }
     return false;
+}
+
+/**
+ * Sets the event listeners for the Classic Editor based on which tab is active.
+ * 
+ * @since 3.3.4
+ * 
+ * @param string functionName The name of the function that has to be called when the event is triggered.
+ */
+function aioseopSetClassicEditorEventListener(functionName) {
+    if (aioseopIsVisualTab()) {
+        setTimeout(function () {
+            tinymce.editors[0].on('KeyUp', function () {
+                functionName();
+            });
+        }, 500);
+    } else {
+        setTimeout(function () {
+            $('.wp-editor-area').on('change', function () {
+                functionName();
+            });
+        }, 500);
+    }
+}
+
+/**
+ * Sets the event listener for the editor tab switch.
+ * 
+ * @since 3.3.4
+ * 
+ * @param string functionName The name of the function that needs to be called when the event is triggered.
+ */
+function aioseopSetClassicEditorTabSwitchEventListener(functionName) {
+    $('.wp-switch-editor').each(function () {
+        $(this).on('click', function () {
+            setTimeout(function () {
+                aioseopSetClassicEditorEventListener(functionName);
+            });
+        })
+    });
+}
+
+/**
+ * Gets the content of the active Classic Editor tab.
+ * 
+ * @since 3.3.4
+ * 
+ * @return string The content of the active editor tab.
+ */
+function aioseopGetClassicEditorContent() {
+    if (aioseopIsVisualTab()) {
+        //tinymce.activeEditor.getContent();
+        return $('#content_ifr').contents().find('body')[0].innerHTML;
+    }
+    return $('.wp-editor-area').val();
+}
+
+/**
+ * Sets the event listener for the Gutenberg Editor.
+ * 
+ * @since 3.3.0
+ * @since 3.4.0 Moved to its own function.
+ * 
+ * @param functionName The name of the function that needs to be called when the event is triggered.
+ */
+function aioseopSetGutenbergEditorEventListener(functionName) {
+    window._wpLoadBlockEditor.then(function () {
+        setTimeout(function () {
+            // https://developer.wordpress.org/block-editor/packages/packages-data/
+            wp.data.subscribe(function () {
+                clearTimeout(aioseopGutenbergEventTimeout);
+                // This is needed because the code otherwise is triggered dozens of times.
+                var aioseopGutenbergEventTimeout = setTimeout(function () {
+                    functionName();
+                }, 200);
+            });
+        });
+    });
 }
