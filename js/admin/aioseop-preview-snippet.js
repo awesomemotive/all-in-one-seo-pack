74c59b434a15243940525ec2fc1f089a2d9903c9
Arnaud Broes <abroes@outlook.be>
2019-11-26 22:54:27 +0100

    Improve regex to remove new lines #3023

--- js/admin/aioseop-preview-snippet.js
+++ js/admin/aioseop-preview-snippet.js
@@ -165,7 +165,7 @@ jQuery(function($){
 		// Remove all HTML tags.
 		content = content.replace(/(<[^ >][^>]*>)?/gm, '');
 		// Remove all line breaks.
-		content = content.replace(/\s\s+/g, ' ');
+		content = content.replace(/[\r\n]+/gm, ' ');
 		return aioseopDecodeHtmlEntities(content.trim());
 	}
 
