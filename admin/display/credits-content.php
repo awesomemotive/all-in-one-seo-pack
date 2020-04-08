2c2b83e432bac1e40f900c5bfcc9f7fa9eef00d4
Alexander Leidinger <Alexander@Leidinger.net>
2020-04-08 19:22:04 +0200

    Security and performance improvement: rel="noopener noreferrer"
    For all external href with target="_blank" to add
    rel="noopener noreferrer". The reason for this patch is performance
    and security, you can find more info at
        https://developers.google.com/web/tools/lighthouse/audits/noopener
    and
        https://jakearchibald.com/2016/performance-benefits-of-rel-noopener/

--- admin/display/credits-content.php
+++ admin/display/credits-content.php
@@ -23,7 +23,7 @@
 <h3 class="wp-people-group"><?php _e( 'Project Leaders', 'all-in-one-seo-pack' ); ?></h3>
 <ul class="wp-people-group " id="wp-people-group-project-leaders">
 	<li class="wp-person" id="wp-person-stevemortiboy">
-		<a class="web" target="_blank" href="https://twitter.com/wpsmort"><img alt="" class="gravatar" src="https://www.gravatar.com/avatar/40e33d813c16a63500675d851b0cbf3a?s=60">
+		<a class="web" target="_blank" rel="noopener noreferrer" href="https://twitter.com/wpsmort"><img alt="" class="gravatar" src="https://www.gravatar.com/avatar/40e33d813c16a63500675d851b0cbf3a?s=60">
 			Steve Mortiboy</a>
 		<span class="title"><?php _e( 'Project Manager', 'all-in-one-seo-pack' ); ?></span>
 	</li>
@@ -32,12 +32,12 @@
 <h3 class="wp-people-group"><?php printf( __( 'Core Team', 'all-in-one-seo-pack' ) ); ?></h3>
 <ul class="wp-people-group " id="wp-people-group-contributors">
 	<li class="wp-person" id="wp-person-arnaudbroes">
-		<a class="web" target="_blank" href="https://profiles.wordpress.org/arnaudbroes"><img alt="" class="gravatar" src="https://www.gravatar.com/avatar/0ce0d554c2b0bd61d326e15c8dcde756?s=60">
+		<a class="web" target="_blank" rel="noopener noreferrer" href="https://profiles.wordpress.org/arnaudbroes"><img alt="" class="gravatar" src="https://www.gravatar.com/avatar/0ce0d554c2b0bd61d326e15c8dcde756?s=60">
 			Arnaud Broes</a>
 		<span class="title"><?php _e( 'Team Lead', 'all-in-one-seo-pack' ); ?></span>
 	</li>
 	<li class="wp-person" id="EkoJR">
-		<a class="web" target="_blank" href="https://profiles.wordpress.org/EkoJR/"><img alt="" class="gravatar" src="https://secure.gravatar.com/avatar/bb4c78fe944b58bd5f127d836500c30a?s=200&d=mm&r=g">
+		<a class="web" target="_blank" rel="noopener noreferrer" href="https://profiles.wordpress.org/EkoJR/"><img alt="" class="gravatar" src="https://secure.gravatar.com/avatar/bb4c78fe944b58bd5f127d836500c30a?s=200&d=mm&r=g">
 			Ben Reames</a>
 		<span class="title"><?php _e( 'Development Team', 'all-in-one-seo-pack' ); ?></span>
 	</li>
@@ -51,49 +51,49 @@ printf( _e( 'Translation contributors and translation editors', 'all-in-one-seo-
 ?>
 </h3>
 <p class="wp-credits-list">
-	<a href="https://profiles.wordpress.org/pierrelannoy/" target="_blank">Pierre Lannoy</a>,
-	<a href="https://profiles.wordpress.org/sonjanyc/" target="_blank">Sonja Leix</a>,
-	<a href="https://profiles.wordpress.org/dev-ide/" target="_blank">Adil El hallaoui</a>,
-	<a href="https://profiles.wordpress.org/simonie/" target="_blank">simonie</a>,
-	<a href="https://profiles.wordpress.org/lenasterg/" target="_blank">lenasterg</a>,
-	<a href="https://profiles.wordpress.org/arnaudbroes/" target="_blank">Arnaud Broes</a>,
-	<a href="https://profiles.wordpress.org/pixolin/" target="_blank">Bego Mario Garde</a>,
-	<a href="https://profiles.wordpress.org/wp-yogi/" target="_blank">wp-yogi</a>,
-	<a href="https://profiles.wordpress.org/wpsmort/" target="_blank">Steve Mortiboy</a>,
-	<a href="https://profiles.wordpress.org/webaware/" target="_blank">webaware</a>,
-	<a href="https://profiles.wordpress.org/escribirelmundo/" target="_blank">escribirelmundo</a>,
-	<a href="https://profiles.wordpress.org/casiepa/" target="_blank">Pascal Casier</a>,
-	<a href="https://profiles.wordpress.org/shoheitanaka/" target="_blank">Shohei Tanaka</a>,
-	<a href="https://profiles.wordpress.org/nurron/" target="_blank">Nurron Shodiqin</a>,
-	<a href="https://profiles.wordpress.org/aprmndr/" target="_blank">Alyssa Primandaru</a>,
-	<a href="https://profiles.wordpress.org/facestoro/" target="_blank">facestoro</a>,
-	<a href="https://profiles.wordpress.org/yuqianl/" target="_blank">Dawa Torbert</a>,
-	<a href="https://profiles.wordpress.org/hallsofmontezuma/" target="_blank">Michael Torbert</a>,
-	<a href="https://profiles.wordpress.org/istvanzseller/" target="_blank">Istvan Zseller</a>,
-	<a href="https://profiles.wordpress.org/paaljoachim" target="_blank">Paal Joachim Romdahl</a>,
-	<a href="https://profiles.wordpress.org/almaz/" target="_blank">Almaz Mannanov</a>,
-	<a href="https://profiles.wordpress.org/vide13 /" target="_blank">vide13</a>,
-	<a href="https://profiles.wordpress.org/yuraz/" target="_blank">Jurica Zuanovic</a>,
-	<a href="https://profiles.wordpress.org/arhipaiva/" target="_blank">arhipaiva</a>,
-	<a href="https://profiles.wordpress.org/maximanikin/" target="_blank">Maxim Anikin</a>,
-	<a href="https://profiles.wordpress.org/petya/" target="_blank">Petya Raykovska</a>,
-	<a href="https://profiles.wordpress.org/hathanh0809/" target="_blank">hathanh0809</a>,
-	<a href="https://profiles.wordpress.org/cedric3131/" target="_blank">Cédric Valmary</a>,
-	<a href="https://profiles.wordpress.org/smitka/" target="_blank">Vladimir Smitka</a>,
-	<a href="https://profiles.wordpress.org/brewtal/" target="_blank">Paul P.</a>,
-	<a href="https://profiles.wordpress.org/wpaleks/" target="_blank">Aleksander Savkovic</a>,
-	<a href="https://profiles.wordpress.org/diogosanches/" target="_blank">Diogo Sanches</a>,
-	<a href="https://profiles.wordpress.org/klemenfajs/" target="_blank">Klemen Fajs</a>,
-	<a href="https://profiles.wordpress.org/adriancastellanos/" target="_blank">Adrian Castellanos</a>,
-	<a href="https://profiles.wordpress.org/exilhamburger/" target="_blank">exilhamburger</a>,
-	<a href="https://profiles.wordpress.org/garyj/" target="_blank">Gary Jones</a>,
-	<a href="https://profiles.wordpress.org/fernandot/" target="_blank">Fernando Tellado</a>,
-	<a href="https://profiles.wordpress.org/hiwhatsup/" target="_blank">Carlos Zuniga</a>,
-	<a href="https://profiles.wordpress.org/fxbenard/" target="_blank">François Bernard</a>,
-	<a href="https://profiles.wordpress.org/jack0falltrades/" target="_blank">jack0falltrades</a>,
-	<a href="https://profiles.wordpress.org/dancaragea/" target="_blank">Dan Caragea</a>,
-	<a href="https://profiles.wordpress.org/kyla81975/" target="_blank">kyla81975</a>,
-	<a href="https://profiles.wordpress.org/arildknudsen1/" target="_blank">Arild Knudsen</a>.
+	<a href="https://profiles.wordpress.org/pierrelannoy/" target="_blank" rel="noopener noreferrer">Pierre Lannoy</a>,
+	<a href="https://profiles.wordpress.org/sonjanyc/" target="_blank" rel="noopener noreferrer">Sonja Leix</a>,
+	<a href="https://profiles.wordpress.org/dev-ide/" target="_blank" rel="noopener noreferrer">Adil El hallaoui</a>,
+	<a href="https://profiles.wordpress.org/simonie/" target="_blank" rel="noopener noreferrer">simonie</a>,
+	<a href="https://profiles.wordpress.org/lenasterg/" target="_blank" rel="noopener noreferrer">lenasterg</a>,
+	<a href="https://profiles.wordpress.org/arnaudbroes/" target="_blank" rel="noopener noreferrer">Arnaud Broes</a>,
+	<a href="https://profiles.wordpress.org/pixolin/" target="_blank" rel="noopener noreferrer">Bego Mario Garde</a>,
+	<a href="https://profiles.wordpress.org/wp-yogi/" target="_blank" rel="noopener noreferrer">wp-yogi</a>,
+	<a href="https://profiles.wordpress.org/wpsmort/" target="_blank" rel="noopener noreferrer">Steve Mortiboy</a>,
+	<a href="https://profiles.wordpress.org/webaware/" target="_blank" rel="noopener noreferrer">webaware</a>,
+	<a href="https://profiles.wordpress.org/escribirelmundo/" target="_blank" rel="noopener noreferrer">escribirelmundo</a>,
+	<a href="https://profiles.wordpress.org/casiepa/" target="_blank" rel="noopener noreferrer">Pascal Casier</a>,
+	<a href="https://profiles.wordpress.org/shoheitanaka/" target="_blank" rel="noopener noreferrer">Shohei Tanaka</a>,
+	<a href="https://profiles.wordpress.org/nurron/" target="_blank" rel="noopener noreferrer">Nurron Shodiqin</a>,
+	<a href="https://profiles.wordpress.org/aprmndr/" target="_blank" rel="noopener noreferrer">Alyssa Primandaru</a>,
+	<a href="https://profiles.wordpress.org/facestoro/" target="_blank" rel="noopener noreferrer">facestoro</a>,
+	<a href="https://profiles.wordpress.org/yuqianl/" target="_blank" rel="noopener noreferrer">Dawa Torbert</a>,
+	<a href="https://profiles.wordpress.org/hallsofmontezuma/" target="_blank" rel="noopener noreferrer">Michael Torbert</a>,
+	<a href="https://profiles.wordpress.org/istvanzseller/" target="_blank" rel="noopener noreferrer">Istvan Zseller</a>,
+	<a href="https://profiles.wordpress.org/paaljoachim" target="_blank" rel="noopener noreferrer">Paal Joachim Romdahl</a>,
+	<a href="https://profiles.wordpress.org/almaz/" target="_blank" rel="noopener noreferrer">Almaz Mannanov</a>,
+	<a href="https://profiles.wordpress.org/vide13 /" target="_blank" rel="noopener noreferrer">vide13</a>,
+	<a href="https://profiles.wordpress.org/yuraz/" target="_blank" rel="noopener noreferrer">Jurica Zuanovic</a>,
+	<a href="https://profiles.wordpress.org/arhipaiva/" target="_blank" rel="noopener noreferrer">arhipaiva</a>,
+	<a href="https://profiles.wordpress.org/maximanikin/" target="_blank" rel="noopener noreferrer">Maxim Anikin</a>,
+	<a href="https://profiles.wordpress.org/petya/" target="_blank" rel="noopener noreferrer">Petya Raykovska</a>,
+	<a href="https://profiles.wordpress.org/hathanh0809/" target="_blank" rel="noopener noreferrer">hathanh0809</a>,
+	<a href="https://profiles.wordpress.org/cedric3131/" target="_blank" rel="noopener noreferrer">Cédric Valmary</a>,
+	<a href="https://profiles.wordpress.org/smitka/" target="_blank" rel="noopener noreferrer">Vladimir Smitka</a>,
+	<a href="https://profiles.wordpress.org/brewtal/" target="_blank" rel="noopener noreferrer">Paul P.</a>,
+	<a href="https://profiles.wordpress.org/wpaleks/" target="_blank" rel="noopener noreferrer">Aleksander Savkovic</a>,
+	<a href="https://profiles.wordpress.org/diogosanches/" target="_blank" rel="noopener noreferrer">Diogo Sanches</a>,
+	<a href="https://profiles.wordpress.org/klemenfajs/" target="_blank" rel="noopener noreferrer">Klemen Fajs</a>,
+	<a href="https://profiles.wordpress.org/adriancastellanos/" target="_blank" rel="noopener noreferrer">Adrian Castellanos</a>,
+	<a href="https://profiles.wordpress.org/exilhamburger/" target="_blank" rel="noopener noreferrer">exilhamburger</a>,
+	<a href="https://profiles.wordpress.org/garyj/" target="_blank" rel="noopener noreferrer">Gary Jones</a>,
+	<a href="https://profiles.wordpress.org/fernandot/" target="_blank" rel="noopener noreferrer">Fernando Tellado</a>,
+	<a href="https://profiles.wordpress.org/hiwhatsup/" target="_blank" rel="noopener noreferrer">Carlos Zuniga</a>,
+	<a href="https://profiles.wordpress.org/fxbenard/" target="_blank" rel="noopener noreferrer">François Bernard</a>,
+	<a href="https://profiles.wordpress.org/jack0falltrades/" target="_blank" rel="noopener noreferrer">jack0falltrades</a>,
+	<a href="https://profiles.wordpress.org/dancaragea/" target="_blank" rel="noopener noreferrer">Dan Caragea</a>,
+	<a href="https://profiles.wordpress.org/kyla81975/" target="_blank" rel="noopener noreferrer">kyla81975</a>,
+	<a href="https://profiles.wordpress.org/arildknudsen1/" target="_blank" rel="noopener noreferrer">Arild Knudsen</a>.
 </p>
 
 </div>
--- admin/display/welcome-content.php
+++ admin/display/welcome-content.php
@@ -11,7 +11,7 @@
 	<div class="welcome-panel-content">
 		<div class="welcome-panel-column-container">
 			<div>
-				<h3><a href="https://semperplugins.com/all-in-one-seo-pack-3-4/" target="_blank"><?php echo esc_html( __( "Check out what's new in our latest release post!", 'all-in-one-seo-pack' ) ); ?></a></h3>
+				<h3><a href="https://semperplugins.com/all-in-one-seo-pack-3-4/" target="_blank" rel="noopener noreferrer"><?php echo esc_html( __( "Check out what's new in our latest release post!", 'all-in-one-seo-pack' ) ); ?></a></h3>
 			</div>
 			<div class="welcome-panel-column">
 				<h3>
@@ -35,7 +35,7 @@
 				<p class="call-to-action">
 					<a
 						href="https://semperplugins.com/all-in-one-seo-pack-pro-version/?loc=aio_welcome"
-						target="_blank"
+						target="_blank" rel="noopener noreferrer"
 						class="button button-primary button-orange"><?php echo __( 'Upgrade', 'all-in-one-seo-pack' ); ?></a>
 				</p>
 				<p class="message aioseop-message welcome-icon welcome-edit-page">
@@ -48,7 +48,7 @@
 					<a
 						href="https://translate.wordpress.org/projects/wp-plugins/all-in-one-seo-pack"
 						class="button button-primary"
-						target="_blank"><?php echo __( 'Translate', 'all-in-one-seo-pack' ); ?></a></p>
+						target="_blank" rel="noopener noreferrer"><?php echo __( 'Translate', 'all-in-one-seo-pack' ); ?></a></p>
 			</div>
 
 			<div class="welcome-panel-column">
@@ -57,7 +57,7 @@
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/quick-start-guide/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-add-page">
 							<?php
 							/* translators: %s is a placeholder, which means that it should not be translated. It will be replaced with the name of the plugin, All in One SEO Pack. */
@@ -69,37 +69,37 @@
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/beginners-guide-to-xml-sitemaps/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-add-page"><?php echo __( 'Beginners Guide for XML Sitemap module', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/beginners-guide-to-social-meta/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-add-page"><?php echo __( 'Beginners Guide for Social Meta module', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/top-tips-for-good-on-page-seo/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-add-page"><?php echo __( 'Tips for good on-page SEO', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/quality-guidelines-for-seo-titles-and-descriptions/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-add-page"><?php echo __( 'Quality guidelines for SEO titles and descriptions', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/submitting-an-xml-sitemap-to-google/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-add-page"><?php echo __( 'Submit an XML Sitemap to Google', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/setting-up-google-analytics/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-add-page"><?php echo __( 'Set up Google Analytics', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 				</ul>
@@ -111,20 +111,20 @@
 					<li>
 						<a
 							href="https://semperplugins.com/documentation/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-learn-more"><?php echo __( 'We have complete documentation on every setting and feature', 'all-in-one-seo-pack' ); ?></a>
 
 					</li>
 					<li>
 						<a
 							href="https://semperplugins.com/videos/"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-learn-more"><?php echo __( 'Access to video tutorials about SEO with the Pro version', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 					<li>
 						<a
 							href="https://semperplugins.com/all-in-one-seo-pack-pro-version/?loc=aio_welcome"
-							target="_blank"
+							target="_blank" rel="noopener noreferrer"
 							class="welcome-icon welcome-learn-more"><?php echo __( 'Control SEO on categories, tags and custom taxonomies with the Pro version', 'all-in-one-seo-pack' ); ?></a>
 					</li>
 				</ul>
