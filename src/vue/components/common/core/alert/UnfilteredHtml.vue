ed6822355330e6e8176e965e7ad83f0848fceec2
Arnaud Broes <abroes@outlook.be>
2023-06-29 15:07:56 +0200

    4.4.0.3

---
+++ src/vue/components/common/core/alert/UnfilteredHtml.vue
@@ -0,0 +1,36 @@
+<template>
+	<core-alert
+		class="no-access"
+		v-if="!rootStore.aioseo.user.unfilteredHtml"
+		type="red"
+		v-html="strings.unfilteredHtmlError"
+	/>
+</template>
+<script>
+import {
+	useRootStore
+} from '@/vue/stores'
+
+import CoreAlert from '@/vue/components/common/core/alert/Index'
+export default {
+	setup () {
+		return {
+			rootStore : useRootStore()
+		}
+	},
+	components : {
+		CoreAlert
+	},
+	data () {
+		return {
+			strings : {
+				unfilteredHtmlError : this.$t.sprintf(
+					// Translators: 1 - Learn more link.
+					this.$t.__('Your user account role does not have access to edit this field. %1$s', this.$td),
+					this.$links.getDocLink(this.$constants.GLOBAL_STRINGS.learnMore, 'unfilteredHtml', true)
+				)
+			}
+		}
+	}
+}
+</script>
